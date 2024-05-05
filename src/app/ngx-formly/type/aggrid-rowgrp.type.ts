6165c812232419cb365528a07031965ca5f4ce94
mahadeo.khamgal <mahadeo.khamgal@infosys.com>
2024-05-06 00:06:04 +0530

    row grouping worked.

--- src/app/ngx-formly/type/aggrid-rowgrp.type.ts
+++ src/app/ngx-formly/type/aggrid-rowgrp.type.ts
@@ -2,29 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import { FieldArrayType } from '@ngx-formly/core';
 import { FirstDataRenderedEvent, GridOptions, ColDef } from 'ag-grid-community';
 import { GridFormlyCellComponent } from './grid-formly-cell.component';
+// import * as _ from 'lodash';
 @Component({
-    selector: 'formly-field-grid', 
-    templateUrl:'./aggrid-rowgrp.type.html',
+    selector: 'formly-field-grid',
+    templateUrl: './aggrid-rowgrp.type.html',
 })
 
 export class RowGroupTypeComponent extends FieldArrayType implements OnInit {
     gridOptions: GridOptions | undefined; style: any = {};
+    // gridOptionsLocal: GridOptions[] | undefined;
     groupedData: any[] = [];
+    groupedDataLocal: any[] = [];
     ngOnInit() {
-        console.log('the props is',this.props)
-        
+        console.log('the props is', this.props);
+        console.log("model is", this.model);
+
+
         this.style = { width: this.props['width'], height: this.props['height'], }; // map cell Renderer to Formly Component this.props.gridOptions.columnDefs.forEach((column: ColDef) => { column.cellRenderer = GridFormlyCellComponent; }); // set grid options and context of the parent formly field 
         const gridOptions: GridOptions = this.props['gridOptions'] || {};
         gridOptions.context = { parentField: this.field, };
         this.gridOptions = gridOptions;
-       
+
+        this.groupedData = this.groupBy(this.model, 'investmentDate') as any;
+        Object.entries(this.groupedData).forEach((group: any, key) => {
+            console.log("grouped data", group, key);
+            this.groupedDataLocal[key] = {
+                'title': group[0],
+                'data': group[1],
+            }
+        })
     }
-    onFirstDataRendered(params: FirstDataRenderedEvent) { 
+    onFirstDataRendered(params: FirstDataRenderedEvent) {
         params.api.sizeColumnsToFit();
-        console.log('grouped data is',this.groupBy(this.props['gridOptions']['rowData'],'investmentDate'));
-     }
+        console.log('grouped data is', this.groupBy(this.props['gridOptions']['rowData'], 'investmentDate'));
+    }
     groupBy<T>(array: T[], key: keyof T): { [key: string]: T[] } {
-        return array.reduce((acc : any, current) => {
+        return array.reduce((acc: any, current) => {
             const keyName = current[key];
             if (!acc[keyName]) {
                 acc[keyName] = [];
@@ -33,13 +46,13 @@ export class RowGroupTypeComponent extends FieldArrayType implements OnInit {
             return acc;
         }, {});
     }
-    getGroupHeaders():any[]{
-        if(!this.props['gridOptions']['rowData']) {
+    getGroupHeaders(): any[] {
+        if (!this.props['gridOptions']['rowData']) {
             this.groupedData = [];
             return [];
         }
-        this.groupedData = this.groupBy(this.props['gridOptions']['rowData'],'investmentDate') as any;
+        this.groupedData = this.groupBy(this.props['gridOptions']['rowData'], 'investmentDate') as any;
         return Object.keys(this.groupedData);
     }
- 
+
 }
